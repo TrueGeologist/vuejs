1c711d61c1fe7cdc4b71939ba6e66a00d52ffc86
Mukhametzyanov Marat <m.mar.rust@gmail.com>
2021-03-30 02:45:22 +0300

    hw4 done

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,46 @@
+<template>
+<li class="catalog__item">
+  <a class="catalog__pic" href="#">
+    <img :src="product.image" :alt="product.title">
+  </a>
+
+  <h3 class="catalog__title">
+    <a href="#">
+      {{ product.title }}
+    </a>
+  </h3>
+
+  <span class="catalog__price">
+    {{ product.price }} ₽
+  </span>
+
+  <ul class="colors colors--black">
+    <li class="colors__item" v-for="(colors, index) in product.colors" :key="colors.color">
+      <label class="colors__label">
+        <input class="colors__radio sr-only" type="radio"
+        :value="value(index)" v-model="checkedColor">
+        <span class="colors__value" :style="style(index)"></span>
+      </label>
+    </li>
+  </ul>
+</li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      checkedColor: 3,
+    };
+  },
+  props: ['product'],
+  methods: {
+    value(i) {
+      return this.product.colors[i].id;
+    },
+    style(i) {
+      return `backgroundColor: ${this.product.colors[i].code}`;
+    },
+  },
+};
+</script>
--- src/data/products.js
+++ src/data/products.js
@@ -1,62 +1,166 @@
 export default [
   {
+    id: 1,
+    categoryId: 1,
     title: 'Игровой ноутбук MSI GF65 Thin 9SEXR-691RU (9S7-16W112-691)',
     price: 89990,
     image: 'img/laptop-msi.jpg',
+    colors: [
+      {
+        id: 1,
+        code: '#222',
+      },
+    ],
   },
   {
+    id: 2,
+    categoryId: 1,
     title: 'Ноутбук APPLE MacBook Air M1 13.3", IPS, Apple M1 8ГБ, 512ГБ SSD, Mac OS, MGNA3RU/A, серебристый',
     price: 123990,
     image: 'img/laptop-macbook.jpg',
+    colors: [
+      {
+        id: 2,
+        code: '#939393',
+      },
+    ],
   },
   {
+    id: 3,
+    categoryId: 2,
     title: 'Беспроводные наушники Apple AirPods 2 без беспроводной зарядки White (MV7N2RU/A)',
     price: 12790,
     image: 'img/headphones-airpods.jpg',
+    colors: [
+      {
+        id: 3,
+        code: '#FFF',
+      },
+    ],
   },
   {
+    id: 4,
+    categoryId: 2,
     title: 'Беспроводные наушники Apple AirPods Pro White (MWP22RU/A)',
     price: 19490,
     image: 'img/headphones-airpodsPro.jpg',
+    colors: [
+      {
+        id: 3,
+        code: '#FFF',
+      },
+    ],
   },
   {
+    id: 5,
+    categoryId: 2,
     title: 'Беспроводные наушники Xiaomi Redmi AirDots Black',
     price: 1350,
     image: 'img/headphones-xiaomi.jpg',
+    colors: [
+      {
+        id: 1,
+        code: '#222',
+      },
+    ],
   },
   {
+    id: 6,
+    categoryId: 3,
     title: 'Игровая приставка Sony Playstation 5 825Gb',
     price: 46990,
     image: 'img/sonyPlaystation5Cd.jpg',
+    colors: [
+      {
+        id: 3,
+        code: '#FFF',
+      },
+    ],
   },
   {
+    id: 7,
+    categoryId: 3,
     title: 'Игровая приставка Sony PlayStation 5 Digital Edition',
     price: 37990,
     image: 'img/sonyPlaystation5DE.jpg',
+    colors: [
+      {
+        id: 3,
+        code: '#FFF',
+      },
+    ],
   },
   {
+    id: 8,
+    categoryId: 3,
     title: 'Игровая приставка Microsoft Xbox Series X',
     price: 45590,
     image: 'img/xboxX.jpg',
+    colors: [
+      {
+        id: 1,
+        code: '#222',
+      },
+    ],
   },
   {
+    id: 9,
+    categoryId: 4,
     title: 'Смарт-часы Apple Watch Series 6 44mm Space Grey with Black Sport Band (M00H3RU/A)',
     price: 39490,
     image: 'img/appleWatch6.jpg',
+    colors: [
+      {
+        id: 1,
+        code: '#222',
+      },
+      {
+        id: 2,
+        code: '#939393',
+      },
+    ],
   },
   {
+    id: 10,
+    categoryId: 4,
     title: 'Смарт-часы Samsung Galaxy Watch Active 2 Gold/Gold (SM-R830NZDASER)',
     price: 17491,
     image: 'img/samsungWatchActive2.jpg',
+    colors: [
+      {
+        id: 1,
+        code: '#222',
+      },
+      {
+        id: 4,
+        code: '#FF6B00',
+      },
+    ],
   },
   {
+    id: 11,
+    categoryId: 5,
     title: 'LED Телевизор HD Ready Novex NWX-24H121MSY',
     price: 8990,
     image: 'img/ya-tv.jpg',
+    colors: [
+      {
+        id: 2,
+        code: '#939393',
+      },
+    ],
   },
   {
+    id: 12,
+    categoryId: 5,
     title: 'LED телевизор 4K Ultra HDSamsung UE55TU7570U',
     price: 57990,
     image: 'img/samsung-tv.jpg',
+    colors: [
+      {
+        id: 1,
+        code: '#222',
+      },
+    ],
   },
 ];
