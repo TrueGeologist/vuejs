c1bd43373329e7a6f92035facb16d7a1aec45508
Mukhametzyanov Marat <m.mar.rust@gmail.com>
2021-04-05 18:30:48 +0300

    hw4 done

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,36 @@
+<template>
+<li class="catalog__item">
+  <a class="catalog__pic" href="#">
+    <img :src="product.image" :alt="product.title">
+  </a>
+
+  <h3 class="catalog__title">
+    <a href="#">
+      {{ product.title }}
+    </a>
+  </h3>
+
+  <span class="catalog__price">
+    {{ product.price }} ₽
+  </span>
+
+  <ColorsList class="colors--black"
+    :colors="product.colors"
+    :checkedColor.sync="checkedColorId"
+  />
+</li>
+</template>
+
+<script>
+import ColorsList from '@/components/ColorsList.vue';
+
+export default {
+  data() {
+    return {
+      checkedColorId: 3,
+    };
+  },
+  props: ['product'],
+  components: { ColorsList },
+};
+</script>
---
+++ src/data/colors.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1,
+    code: '#222',
+  },
+  {
+    id: 2,
+    code: '#939393',
+  },
+  {
+    id: 3,
+    code: '#FFF',
+  },
+  {
+    id: 4,
+    code: '#FF6B00',
+  },
+];
