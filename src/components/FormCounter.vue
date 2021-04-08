f6b19e34814e7cad75450890bdb38419903adaee
Mukhametzyanov Marat <m.mar.rust@gmail.com>
2021-04-08 19:32:18 +0300

    hw6 changed

---
+++ src/components/FormCounter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button type="button" aria-label="Убрать один товар"
+      @click="localAmount>1 ? localAmount-- : localAmount">
+      <svg fill="currentColor">
+        <use xlink:href="#icon-minus"></use>
+      </svg>
+    </button>
+
+    <input type="number" min="1" v-model.number="localAmount">
+
+    <button type="button" aria-label="Добавить один товар"
+      @click="localAmount++">
+      <svg fill="currentColor">
+        <use xlink:href="#icon-plus"></use>
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      localAmount: this.amount,
+    };
+  },
+  props: ['amount'],
+  watch: {
+    localAmount() {
+      this.$emit('update:amount', this.localAmount);
+    },
+  },
+};
+</script>
